f4b2777542c2cd9822657fc5dba699160c928c25
moaz.magdy <44733972+moazmagdy@users.noreply.github.com>
2022-07-10 20:24:06 +0200

    modify

--- Climbing_Stairs_Problem (DP + Iteration) #70.py
+++ Climbing_Stairs_Problem (DP + Iteration) #70.py
@@ -19,6 +19,8 @@ Explanation: There are three ways to climb to the top.
 1. 1 step + 1 step + 1 step
 2. 1 step + 2 steps
 3. 2 steps + 1 step
+
+This solution implement dynamic programming with iteration.
 """
 # from functools import lru_cache
 
---
+++ Climbing_Stairs_Problem (DP + Recursion) #70.py
@@ -0,0 +1,38 @@
+"""
+LeetCode - Climbing Stairs #70
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+This solution implement dynamic programming with recursion.
+"""
+# from functools import lru_cache
+
+# @lru_cache()
+
+cache = {}
+def climbStairs(n):
+    if n in cache:
+        return cache[n]
+    if n == 1 or n == 2 or n == 3:
+        cache[n] = n
+        return cache[n]
+    else:
+        cache[n] = climbStairs(n -1) + climbStairs(n-2)
+        return cache[n]
